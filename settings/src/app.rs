2009bd09b6e6448957abe1d7fef0c7f17ab9f756
Jonathan Capps <cappsy@gmail.com>
2025-05-27 15:51:24 +0100

    chore: settings about page cleanup

--- settings/src/app.rs
+++ settings/src/app.rs
@@ -496,7 +496,7 @@ impl cosmic::Application for AppModel {
 impl AppModel {
     pub fn about(&self) -> Element<Message> {
         let cosmic_theme::Spacing { space_xxs, .. } = theme::active().cosmic().spacing;
-        let icon = widget::svg(widget::svg::Handle::from_memory(APP_ICON));
+        let icon = widget::svg(widget::svg::Handle::from_memory(APP_ICON)).width(250);
         let title = widget::text::title3(fl!("app-title"));
 
         widget::column()
@@ -504,6 +504,7 @@ impl AppModel {
             .push(title)
             .align_x(Alignment::Center)
             .spacing(space_xxs)
+            .width(Length::Fill)
             .into()
     }
 
